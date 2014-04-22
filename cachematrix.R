5ebbbfdb0e5c3b0ff44f7732c604131a22229be9
Miguel Prata <prata.miguel@gmail.com>
2014-04-22 17:44:57 +0100

    teste

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,7 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## TESTE TESTE TESTE
 
 makeCacheMatrix <- function(x = matrix()) {
 
